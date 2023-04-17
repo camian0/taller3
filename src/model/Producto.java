af84c1ab1f02b7d88eae74a957d9927cd75facfb
camian0 <camian6@outlook.com>
2023-04-16 21:34:51 -0500

    the code to do queries into product class was added

--- src/model/Producto.java
+++ src/model/Producto.java
@@ -1,4 +1,44 @@
 package model;
 
 public class Producto {
+
+    private String nombre;
+    private Short edad;
+    private Float precioBase;
+
+    public Producto(String unNombre, Short unaEdad, Float unPrecioBase) {
+        nombre = unNombre;
+        edad = unaEdad;
+        precioBase = unPrecioBase;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Short getEdad() {
+        return edad;
+    }
+
+    public Float getPrecioBase() {
+        return precioBase;
+    }
+
+    public void setNombre(String unNombre) {
+        if (unNombre != null) {
+            nombre = unNombre;
+        }
+    }
+
+    public void setEdad(Short unaEdad) {
+        if (unaEdad != null) {
+            edad = unaEdad;
+        }
+    }
+
+    public void setPrecioBase(Float unPrecio) {
+        if (unPrecio != null) {
+            precioBase = unPrecio;
+        }
+    }
 }
