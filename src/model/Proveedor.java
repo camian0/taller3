bc21d0b1fc32af9abd59bfe639765632cc011c45
camian0 <camian6@outlook.com>
2023-04-16 21:18:12 -0500

    provider class code was added to do the querys into class

--- src/model/Proveedor.java
+++ src/model/Proveedor.java
@@ -1,4 +1,28 @@
 package model;
 
 public class Proveedor {
+
+    private String nombre;
+    private Float precioEnvío;
+
+    public Proveedor(String unNombre, float unPrecioEnvio) {
+        nombre = unNombre;
+        precioEnvío = unPrecioEnvio = unPrecioEnvio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Float getPrecioEnvio() {
+        return precioEnvío;
+    }
+
+    public void setNombre(String unNombre) {
+        nombre = unNombre;
+    }
+
+    public void setPrecioEnvío(float unPrecio) {
+        precioEnvío = unPrecio;
+    }
 }
